1e91fe435d1b50571f2b09f9ba6afc9c2e6dd013
ronek22 <jronek3010@gmail.com>
2019-02-04 14:14:48 +0100

    I think this will work now

--- core/utility.py
+++ core/utility.py
@@ -8,49 +8,12 @@ from pathlib import Path
 import redis
 
 
-def search_files():
-    return [f for f in listdir('history') if isfile(join('history', f))]
-
-
-def get_users_with_files():
-    # TODO: Maybe is possible to make this with only one loop
-    files = search_files()
-    users_diary = {x[:-5].split('_')[1]: {} for x in files}
-    for x in files:
-        user = (x[:-5].split('_'))[1]
-        if 'history' in x:
-            users_diary[user]['history'] = x
-        else:
-            users_diary[user]['deposit'] = x
-
-    return users_diary
-
-
-def choose_user():
-    users = get_users_with_files()
-    for key in users.keys():
-        print(key)
-
-    user = input("Choose user: ")
-    return open_json(users[user]['history']), open_json(users[user]['deposit'])  # tuple(history, deposits)
-
-
 def get_user(db, user):
     return open_json(db.get('history:'+user)), open_json(db.get('deposits:'+user))  # tuple(history, deposits)
 
 
 def cash_format(cash):
     return "{0:.2f}".format(cash)
-
-
-def printf(header, value, value_type='cash'):
-    if 'cash' in value_type:
-        print('{:<35} {:>10} zł'.format(header, cash_format(value)))
-    elif 'percent' in value_type:
-        print('{:<35} {:>10} %'.format(header, cash_format(value)))
-    elif 'bool' in value_type:
-        print('{:<35} {:>10}'.format(header, 'Possible' if True else 'Not possible'))
-
 
 def stringf(header, value, value_type='cash'):
     if 'cash' in value_type:
