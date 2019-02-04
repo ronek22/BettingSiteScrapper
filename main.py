8f017918cf82725c5fc0c751964ea82f420a4273
ronek22 <jronek3010@gmail.com>
2019-02-04 11:23:53 +0100

    Remove sensitive data, conf variables

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from core.runner import Runner
+import os
 
-if __name__ == "__main__":
-    with open('client.secret', 'r') as f:
-        login, password = f.readline().split()
+if __name__ == "__main__": 
+    login, password = os.environ.get('LOGIN'), os.environ.get('PASSWORD')
+    print("LOG: {}\tPASS: {}".format(login, password))
 
     runner = Runner(login, password)
